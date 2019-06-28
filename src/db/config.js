429ef8f46422538a49dbdf4b7559573dea1cd205
charlesagyemang <micnkru@gmail.com>
2019-06-28 12:12:09 +0000

    Initialized project

---
+++ src/db/config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  development: {
+    username: null,
+    password: null,
+    database: 'test-dev',
+    host: 'localhost',
+    dialect: 'postgres',
+    seederStorage: 'json',
+    seederStoragePath: 'src/db/dev_seed_sequelize_data.json',
+  },
+  test: {
+    username: 'postgres',
+    password: 'postgres',
+    database: 'test-test',
+    host: 'localhost',
+    dialect: 'postgres',
+    seederStorage: 'json',
+    seederStoragePath: 'src/db/test_seed_sequelize_data.json',
+  },
+  production: {
+    username: process.env.DB_USER || '',
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || '',
+    host: process.env.DB_HOSTNAME || '',
+    dialect: 'postgres',
+    seederStorage: 'json',
+    seederStoragePath: 'src/db/prod_seed_sequelize_data.json',
+  },
+  stage: {
+    username: process.env.DB_USER || '',
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || '',
+    host: process.env.DB_HOSTNAME || '',
+    dialect: 'postgres',
+    seederStorage: 'json',
+    seederStoragePath: 'src/db/prod_seed_sequelize_data.json',
+  },
+};
---
+++ src/modules/user/user.controller.js
@@ -0,0 +1,46 @@
+import HTTPStatus from 'http-status';
+import sequelize from 'sequelize';
+import User from './user.model';
+
+
+export const getUser = async (req, res) => {
+  const user = await User.findById(id);
+  if (!user) {
+    res.sendStatus(HTTPStatus.NOT_FOUND);
+    return;
+  }
+  res.send(user);
+};
+
+
+export const register = async (req, res, next) => {
+  try {
+    const user = await User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email: invite.email,
+    });
+
+    const u = user.auth();
+    return res.status(HTTPStatus.CREATED).json(u);
+  } catch (ex) {
+    if (err) next(err);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } });
+
+    if (!user || !user.authenticate(req.body.password)) {
+      return res.status(HTTPStatus.NOT_FOUND).json({ message: 'User not found' });
+    }
+
+    const u = user.auth();
+
+    return res.json(u);
+  } catch (ex) {
+    if (err) next(err);
+  }
+};
+
